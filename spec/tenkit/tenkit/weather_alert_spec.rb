85ef91d354d1baf645c7b930c5a349e41f90cbee
John Woodell <woodie@netpress.com>
2025-02-10 08:53:40 -0800

    Clean up the alert spec.

--- spec/tenkit/tenkit/weather_alert_spec.rb
+++ spec/tenkit/tenkit/weather_alert_spec.rb
@@ -2,32 +2,33 @@ require_relative "../spec_helper"
 
 RSpec.describe Tenkit::WeatherAlert do
   let(:client) { Tenkit::Client.new }
-  let(:api_response) { double("WeatherAlertResponse", body: json) }
   let(:json) { File.read("test/fixtures/alert.json") }
-  let(:alert_id) { "0828b382-f63c-4139-9f4f-91a05a4c7cdd" }
+  let(:api_response) { double("WeatherAlertResponse", body: json) }
 
   before { allow(client).to receive(:get).and_return(api_response) }
 
   describe "weather_alert" do
-    let(:msg_head) { "...HEAT ADVISORY REMAINS IN EFFECT UNTIL 7 PM PDT THIS EVENING..." }
-    let(:msg_tail) { "for those working or\nparticipating in outdoor activities." }
+    let(:alert_id) { "0828b382-f63c-4139-9f4f-91a05a4c7cdd" }
 
     subject { client.weather_alert(alert_id).weather_alert.summary }
 
     it "includes expected message" do
       expect(subject.messages.first).to be_a Tenkit::Message
-      expect(subject.messages.first.text).to start_with msg_head
-      expect(subject.messages.first.text).to end_with msg_tail
+      expect(subject.messages.first.language).to eq "en"
+      expect(subject.messages.first.text).to start_with "...HEAT ADVISORY"
+      expect(subject.messages.first.text).to end_with "outdoor activities."
     end
 
-    it "includes expected area" do
+    it "includes expected area feature" do
       expect(subject.area).to be_a Tenkit::Area
+      expect(subject.area.type).to eq "FeatureCollection"
       expect(subject.area.features.first).to be_a Tenkit::Feature
+      expect(subject.area.features.first.type).to eq "Feature"
       expect(subject.area.features.first.geometry).to be_a Tenkit::Geometry
       expect(subject.area.features.first.geometry.type).to eq "Polygon"
       expect(subject.area.features.first.geometry.coordinates).to be_a Tenkit::Coordinates
-      expect(subject.area.features.first.geometry.coordinates.size).to eq 1
-      expect(subject.area.features.first.geometry.coordinates.first.size).to eq 177
+      expect(subject.area.features.first.geometry.coordinates.size).to be 1
+      expect(subject.area.features.first.geometry.coordinates.first.size).to be 177
       expect(subject.area.features.first.geometry.coordinates.first.first).to match [-122.4156, 38.8967]
     end
 
